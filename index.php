ed8622581e8153a73368438417a18e1be4c7f4cf
thereywob <81127870+thereywob@users.noreply.github.com>
2022-04-04 21:14:43 -0400

    Add product card template
    These card will serve as the template for the PHP when displaying products from the database.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>reywob.net</title>
+  <title>Fresno Ski & Snowboard Rental</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
@@ -32,8 +32,49 @@
   </nav>
   <!-- End Navbar -->
 
+  <h1>Rentals</h1>
 
-this is some text
+  <!-- Begin Project Cards -->
+  <div class="album py-5" style="margin-top:-55px">
+    <!--  <div class="container"> -->
+        <hr>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+              <div class="card-body">
+                <h2>Item 1</h2>
+                <p class="card-text">A short description about the product.<br>Make, model, color, size, etc.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a href="#" class="btn btn-sm btn-outline-primary">Reserve</a>
+                    <a href="https://cooktopia.reywob.net" target="_blank" class="btn btn-sm btn-outline-secondary">View</a>
+                  </div>
+                  <small class="text-muted">Available</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+              <div class="card-body">
+                <h2>Item 2</h2>
+                <p class="card-text">A short description about the product.<br>Make, model, color, size, etc.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a href="#" class="btn btn-sm btn-outline-primary">Reserve</a>
+                    <a href="https://cooktopia.reywob.net" target="_blank" class="btn btn-sm btn-outline-secondary">View</a>
+                  </div>
+                  <small class="text-muted">Reserved until 12/31/2022</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  <!-- End Project Cards -->
 
 
 
