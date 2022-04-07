2730cfa22ad5346e8d31fe89c8be7c394eebb0e5
thereywob <81127870+thereywob@users.noreply.github.com>
2022-04-07 18:15:12 -0400

    Add Admin Portal navbar link

--- classes.php
+++ classes.php
@@ -25,7 +25,7 @@
           <a class="nav-link active" href="classes.php">Classes</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Admin Portal</a>
+          <a class="nav-link" href="auth.php">Admin Portal</a>
         </li>
       </ul>
     </div>
