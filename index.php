b3cd1d8ac9458c5415a9dd38b9badf1869478409
Schmaltz <SiriusVisualsBusiness@gmail.com>
2022-04-09 22:01:55 -0400

    add homepage

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Fresno Ski & Snowboard Rental</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <!-- Begin Navbar -->
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">Fresno Ski & Snowboard Rental</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample02">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link active" href="index.php">Rentals</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="lessons.php">Lessons</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Admin Portal</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+
+
+
+<div class="background">
+    <h1>Fresno Ski & Lessons Shop</h1>
+  <img src="images/homepage.png" alt="">
+  <div class="container">
+    <div class="buttons">
+      <a href="lessons.php">
+        <button id="lessons" type="button" name="button">Lessons</button>
+      </a>
+      <a href="shop.php">
+        <button id="shop" type="button" name="button">Shop</button>
+      </a>
+    </div>
+  </div>
+
+
+</div>
+
+
+
+  <!-- Begin Page Content -->
+  <div class="container">
+
+
+  <!-- Begin Project Cards -->
+  <div class="album py-5" style="margin-top:-55px">
+    <!--  <div class="container"> -->
+
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+  <!-- End Project Cards -->
+  </div>
+  <!-- End Page Content -->
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
